e2d54adb8e1e738ff182c539d68dd6762fc5964b
Monteiro Steed <steed.monteiro@gmail.com>
2016-09-15 11:00:15 +0200

    menu animation

--- src/view/menu/MenuView.js
+++ src/view/menu/MenuView.js
@@ -1,49 +1,114 @@
 'use strict';
 
 import React, { Component } from 'react';
-import { View, Text, Image, StyleSheet, TouchableOpacity} from 'react-native';
+import { View, Text, Image, StyleSheet, TouchableOpacity, Dimensions , Animated, Easing} from 'react-native';
 import asset  from '../../app/AppAsset';
 import { Actions } from 'react-native-router-flux';
+import {connect} from 'react-redux';
+
+const {width} = Dimensions.get('window');
+
+class MenuView extends Component {
+
+	constructor(props){
+		super(props);
+		this.state = {
+
+			menu : [
+			{text : 'PAYER', action :  Actions.transfer, left: new Animated.Value(-width) },
+			{text : 'COMPTES', action : Actions.overview, left: new Animated.Value(-width) },
+			{text : 'CONTACTS', action : Actions.contact, left: new Animated.Value(-width)  },
+			{text : 'CARTES', action : Actions.card, left: new Animated.Value(-width)  },
+			{text : 'JOURNAL', action : Actions.journal, left: new Animated.Value(-width)  }]
+		}
+	}
+
+	componentWillReceiveProps(nextProps) {
 
 
-export default class MenuView extends Component {
+		if(	nextProps.menu.location ==  'main'  && nextProps.menu.goTo == 'menu'){
+
+			let animation = [];
+
+			this.state.menu.map((link, index)=>{
+
+				animation.push(
+					Animated.timing(
+					link.left,
+					{
+						toValue: 0,
+						duration : 400,
+						easing : Easing.ease,
+						delay : index * 50
+					})
+				);
+
+			});
+
+			Animated.parallel(animation).start();
+		}
+
+
+
+		if(	nextProps.menu.location ==  'menu'  && nextProps.menu.goTo == 'main'){
+
+			let animation = [];
+
+			this.state.menu.map((link, index)=>{
+
+				animation.push(
+					Animated.timing(
+					link.left,
+					{
+						toValue: -width,
+						duration : 200
+					})
+				);
+
+			});
+
+			Animated.parallel(animation).start();
+		}
+
+	}
+
 
 
 	render(){
 
-		let menu = [
-		{text : 'PAYER', action :  Actions.transfer},
-		{text : 'COMPTES', action : Actions.overview },
-		{text : 'CONTACTS', action : Actions.contact },
-		{text : 'CARTES', action : Actions.card },
-		{text : 'JOURNAL', action : Actions.journal }]
-
 		return (
 			<View style={[style.container, this.props.style]} >
-				{menu.map((item,index)=>{
-					return (<TouchableOpacity  key={index}  onPress={()=>{ this.props.gotTo(item)}}>
-								<Text style={style.title} >{item.text}</Text>
-                            </TouchableOpacity>);
-				})}
-            <TouchableOpacity onPress={()=>{ this.props.gotTo({action: Actions.profile})}}  >
-				<Image source={asset.setting}  style={style.setting} />
-                </TouchableOpacity>
+			{this.state.menu.map((item,index)=>{
+				return (
+					<Animated.View  style={{left: item.left}}>
+					<TouchableOpacity  key={index}  onPress={()=>{ this.props.gotTo(item)}}>
+					<Text style={style.title} >{item.text}</Text>
+					</TouchableOpacity>
+					</Animated.View>
+
+					);
+			})}
+			<TouchableOpacity onPress={()=>{ this.props.gotTo({action: Actions.profile})}}  >
+			<Image source={asset.setting}  style={style.setting} />
+			</TouchableOpacity>
 			</View>
-		);
+			);
 	}
 }
 
 
+
+
 const style = StyleSheet.create({
-    container: {
-    	flex : 1,
-    	flexDirection : 'column',
-        backgroundColor : '#120037',
-        justifyContent: 'flex-start',
-        alignItems: 'center',
-        paddingTop : 70
-    },
-    title : {
+	container: {
+		flex : 1,
+		flexDirection : 'column',
+		backgroundColor : '#120037',
+		justifyContent: 'flex-start',
+		alignItems: 'center',
+		paddingTop : 70
+	},
+	title : {
 		fontFamily : 'Montserrat-Bold',
 		letterSpacing: 10,
 		margin : 30,
@@ -54,3 +119,12 @@ const style = StyleSheet.create({
 		marginTop : 50
 	}
 });
+
+
+function mapStateToProps(state) {
+	return {
+		menu : state.menu
+	};
+}
+
+export default connect(mapStateToProps)(MenuView);
