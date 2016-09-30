ca9076b743eb4c91a8c187bd8e589dcc13fd2588
Gabriel <gabriel.hofman.00@gmail.com>
2016-09-30 10:55:40 +0200

    Filter contacts with scrollwheel or text input

--- src/view/contact/ContactView.js
+++ src/view/contact/ContactView.js
@@ -1,7 +1,6 @@
 'use strict';
-
 import React, { Component } from 'react';
-import { Text, View, ScrollView, ListView, PanResponder, Image, TouchableOpacity, TouchableHighlight, Animated, StyleSheet, Dimensions } from 'react-native';
+import { Text, View, ScrollView, ListView, PanResponder, Image, TouchableOpacity, TouchableHighlight, Animated, StyleSheet, Dimensions, TextInput } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 import AppGuideline from '../../app/AppGuideline';
 import AppAsset from '../../app/AppAsset';
@@ -15,25 +14,25 @@ import {loadContacts} from './ContactAction';
 
 
 const {width, height} = Dimensions.get('window');
-
-const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+const letters = ['', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
 class ContactView extends Component {
-
 	constructor(props) {
 		super(props);
+
+		this.ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
 
 		this.items = [];
 		this.letters = [];
 
 		this.spacerWidth = width / 2 - 35/2;
-
 		this.scrollPos = null;
+		this.letterNb = null;
 
 		this.state = {
-
-			animButton : new Animated.Value(0.00001)
-
+			animButton : new Animated.Value(0.00001),
+			users: this.props.contact.users,
+			filteredUsers: this.props.contact.users
 		};
 
 		for(var i in letters){
@@ -41,9 +40,7 @@ class ContactView extends Component {
 		}
 	}
 
-
 	componentDidMount(){
-
 		//this.headerScroll = this.refs.header.getScrollResponder();
 		this.listViewScroll = this.refs.listView.getScrollResponder();
 
@@ -58,131 +55,173 @@ class ContactView extends Component {
 			}).start();
 	}
 
-
-	save(item){
-		if(this.items[item.props.rowData.name[0]] == undefined || this.items[item.props.rowData.name[0]].layout.y > item.layout.y){
-			this.items[item.props.rowData.name[0]] = item;
-		}
-	}
-
 	saveLetter(item){
 		this.letters[item.props.children] = item;
-
 	}
 
 	onScroll(event){
-
 		this.scrollPos = event.nativeEvent.contentOffset.x;
+		this.letterNb = event.nativeEvent.contentOffset.x / 35;
 
-	}
+		let filteredUsers = [];
 
-	onLetterPress(index){
+		if (letters[this.letterNb] !== undefined) {
+			this.state.users.map((user) => {
+				if (user.givenName.charAt(0) === letters[this.letterNb]) {
+					filteredUsers.push(user)
+				} else if (letters[this.letterNb] === '') {
+					filteredUsers = this.state.users;
+				}
+			})
 
-		let letter = letters[index];
+			console.log(filteredUsers);
 
-		let pos =  this.letters[letter].layout.x - this.spacerWidth ;
-		this.headerScroll.scrollTo({
-			y: 0,
-			x: pos,
-			animated : true
-		});
-
-
-		this.scrollToLetter(index);
-	}
-
-
-	scrollToLetter(index){
-
-		if(letters[index] != undefined){
-			let letter = letters[index];
-
-			if(this.items[letter] != undefined){
-				this.listViewScroll.scrollTo({
-					y: this.items[letter].layout.y,
-					x: 0,
-					animated : true
-				});
-			}else{
-				this.scrollToLetter(index-1);
-			}
-
+			this.setState({
+				...this.state,
+				selectedLetter: letters[this.letterNb],
+				filteredUsers
+			});
 		}
 	}
 
+	// onLetterPress(index){
+	// 	let letter = letters[index];
+	//
+	// 	let pos =  this.letters[letter].layout.x - this.spacerWidth ;
+	// 	this.headerScroll.scrollTo({
+	// 		y: 0,
+	// 		x: pos,
+	// 		animated : true
+	// 	});
+	//
+	// 	this.scrollToLetter(index);
+	// }
+
+	// scrollToLetter(index){
+	// 	if(letters[index] != undefined){
+	// 		let letter = letters[index];
+	//
+	// 		if (this.items[letter] != undefined) {
+	// 			this.listViewScroll.scrollTo({
+	// 				y: this.items[letter].layout.y,
+	// 				x: 0,
+	// 				animated : true
+	// 			});
+	// 		} else {
+	// 			this.scrollToLetter(index-1);
+	// 		}
+	// 	}
+	// }
 
 	openContact(contact) {
 		Actions.contactDetails({contact});
 	}
 
+	save(item) {
+		if (this.items[item.props.rowData.name[0]] == undefined || this.items[item.props.rowData.name[0]].layout.y > item.layout.y) {
+			this.items[item.props.rowData.name[0]] = item;
+		}
+	}
 
-	renderRow(contact){
-
-		if(contact.type !== undefined && contact.type =='action' ){
+	renderRow(contact) {
+		if (contact.type !== undefined && contact.type =='action' ) {
 			return null;
 		}
 
-		return (<ContactItem
-			onPress={this.openContact.bind(this)}
-			rowData={contact}
-			save={this.save.bind(this)}
-			rowData={contact}
-			/>);
-
+		return (
+			<ContactItem
+				onPress={this.openContact.bind(this)}
+				rowData={contact}
+				save={this.save.bind(this)}
+				rowData={contact}
+			/>
+		);
 	}
 
+	searchContacts(value) {
+		let filteredUsers = [];
+
+		this.state.users.map((user) => {
+			let name = `${user.givenName} ${user.familyName}`
+			if (name.indexOf(value) > -1) {
+				filteredUsers.push(user);
+			}
+		});
+
+		this.setState({
+			...this.state,
+			filteredUsers
+		});
+	}
 
 	render() {
+		const dataSource = this.ds.cloneWithRows(this.state.filteredUsers);
+
+		console.log(dataSource);
+		console.log(this.state.users);
 
 		return (
 			<View style={style.container}>
-			<Title style={{color: AppGuideline.colors.deepblue, height: 80}} >CONTACTS</Title>
-			{ false && (
-				<View style={{ height: 80, paddingTop:20, backgroundColor: AppGuideline.colors.lightviolet }}>
-				<View style={{ position: 'absolute',  backgroundColor: '#998BB8', left: width / 2 - 35/2, top: 22.5, width:35, height: 35 , borderRadius: 20}}></View>
-				<ScrollView horizontal={true}
-				scrollEventThrottle={200}
-				onScroll={this.onScroll.bind(this)}
-				ref='header'
-				>
-				<View style={{ width: width / 2 - 35/2, backgroundColor: 'transparent'}} />
-				{letters.map((letter, index)=> {
-					return ( <LetterItem key={index} onPress={()=>{ this.onLetterPress(index) }} save={this.saveLetter.bind(this)}>{letter}</LetterItem>)
-				})}
-				<View style={{ width : width / 2 - 35/2, backgroundColor: 'transparent'}} />
-				</ScrollView>
-
+				<Title style={{color: AppGuideline.colors.deepblue, height: 80}} >CONTACTS</Title>
+				{
+					true && (
+					<View style={{ height: 80, paddingTop:20, backgroundColor: AppGuideline.colors.lightviolet }}>
+						<View style={{ position: 'absolute',  backgroundColor: '#998BB8', left: width / 2 - 35/2, top: 22.5, width:35, height: 35 , borderRadius: 20}}></View>
+						<ScrollView
+							style={{ flex: 1 }}
+							horizontal={true}
+							scrollEventThrottle={200}
+							automaticallyAdjustInsets={false}
+							decelerationRate={0}
+							snapToInterval={35}
+							snapToAlignment='start'
+							showsHorizontalScrollIndicator={false}
+							onScroll={this.onScroll.bind(this)}
+							ref='header'
+						>
+							<View style={{ width: width / 2 - 35/2, backgroundColor: 'transparent'}} />
+								{
+									letters.map((letter, index)=> {
+										return (<LetterItem key={index} onPress={()=>{ console.log(index) }} save={this.saveLetter.bind(this)}>{letter}</LetterItem>)
+									})
+								}
+							<View style={{ width : width / 2 - 35/2, backgroundColor: 'transparent'}} />
+						</ScrollView>
+					</View>
+					)
+				}
+				{
+					false &&
+					<View style={{ flexDirection: 'row', height: 80, paddingTop:20, backgroundColor: AppGuideline.colors.lightviolet }}>
+						<TextInput
+							style={{height: 40, width: width /1.5, borderColor: '#FFF', borderWidth: 1}}
+							autoCapitalize='words'
+							onChangeText={(text) => this.searchContacts(text)}
+						/>
+						<View style={{ flex: 1, marginTop: 13, alignItems: 'center'}}>
+							<Text style={{ color: '#fff' }}>ANNULER</Text>
+						</View>
+					</View>
+				}
+				<View  style={{flex:1}}>
+					{this.props.contact.list.length === 0  && this.props.contact.loading === true && <Text>Chargement...</Text>}
+					<ListView
+						ref='listView'
+						dataSource={dataSource}
+						renderRow={this.renderRow.bind(this)}
+					/>
 				</View>
-				)}
-			<View  style={{flex:1}}>
-
-			{this.props.contact.list.length== 0  && this.props.contact.loading == true && <Text>Chargement...</Text>}
-
-			<ListView
-			ref='listView'
-			dataSource={this.props.contact.bim.dataSource}
-			renderRow={this.renderRow.bind(this)}
-			/>
-
+				<View style={style.addIcon}>
+					<TouchableOpacity onPress={() => {
+						// Actions.addContact();
+					}}>
+						<Animated.Image source={AppAsset.add} style={{ transform: [ {scale :this.state.animButton }] }} />
+					</TouchableOpacity>
+				</View>
 			</View>
-
-			<TouchableOpacity style={style.transferButton} >
-			</TouchableOpacity>
-			<View style={style.addIcon}>
-			<TouchableOpacity >
-			<Animated.Image source={AppAsset.add} style={{ transform: [ {scale :this.state.animButton }] }} />
-			</TouchableOpacity>
-			</View>
-
-
-
-			</View>
-			);
+		);
 	}
-
 }
-
-
 
 const style = StyleSheet.create({
 	container: {
@@ -195,8 +234,6 @@ const style = StyleSheet.create({
 		right : -10
 	}
 });
-
-
 
 function mapStateToProps(state) {
 	return {
